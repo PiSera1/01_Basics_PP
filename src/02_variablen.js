737777a77f9c87ee7669f39b303dcafa58c63e02
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-01-03 09:50:06 +0000

    02 /  Wertzuweisung II + Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -11,10 +11,31 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertzuweisung | assignment
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Max"; // Wertzuweisung | assignment
+// console.log(firstName); // Ausgabe
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log("Hi, ich bin " + firstName + " " + familyName + "!");
 
+/*  
+    Wertzuweisung II + Datentypen 
+    JS ist ein untypisierte Sprache!
+*/
+
+let test; // Deklaration
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("--------------------");
+test = "hello"; // string
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("--------------------");
+test = 2; // number
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("--------------------");
+test = true; // boolean
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("--------------------");

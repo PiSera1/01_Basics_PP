9a9de0506200246ff2341bafb1956d6e57733ba5
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-01-07 08:15:46 +0000

    05 / 2. Iteration Date()

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -7,11 +7,12 @@ const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
 // Berechnung Alter
-
-let year = 2025;
+let date = new Date();
+let year = date.getFullYear();
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
+// console.log("Datum. "  + date);

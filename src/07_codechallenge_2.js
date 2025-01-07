3412f18013ea493d1fa3b2851ad0d3448a9ebf14
PiSera1 <kevinwellens24@gmail.com>
2025-01-07 18:56:35 +0000

    07 / Aufgabe lösung

--- src/07_codechallenge_2.js
+++ src/07_codechallenge_2.js
@@ -19,3 +19,30 @@ Alter 13-17 // Cola
 /* Getränke-Challenge */
 
 const prompt = require('prompt-sync')({sigint: true});
+
+
+// Eingabe des Namens
+const name = prompt('Bitte gebe einen Namen ein: ');
+
+// Eingabe des Alters
+const age = +prompt('Bitte gebe das Alter ein: ');
+
+// Funktion zur Bestimmung des Getränks basierend auf dem Alter
+function getGetraenk(alter) {
+    let getraenk;
+    if (alter > 0 && alter < 6) {
+        getraenk = "Milch";
+    } else if (alter >= 6 && alter <= 12) {
+        getraenk = "Saft";
+    } else if (alter >= 13 && alter <= 17) {
+        getraenk = "Cola";
+    } else if (alter >= 18) {
+        getraenk = "Wein";
+    } else {
+        getraenk = "Ungültiges Alter";
+    }
+    console.log(`${name} trinkt ${getraenk}.`);
+}
+
+// Aufruf der Funktion
+getGetraenk(age, name);

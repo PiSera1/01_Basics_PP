9d6169c08eaf68ea18353ac513d28dd78c7785a9
PiSera1 <kevinwellens24@gmail.com>
2025-01-08 09:16:36 +0000

    08 / 2b. Parametrisierung + Datenübergabe von AUSSEN

--- src/08_funktionen_1.js
+++ src/08_funktionen_1.js
@@ -14,13 +14,22 @@ function test()
 /***** Funktionen 02a *****/
 // 2a. Parametrisierung + Datenübergabe von INNEN
 
-ausgabeNamen(); // call
+// ausgabeNamen(); // call
 
 function ausgabeNamen() {
     // interne Variable | what happens in VEGAS ...
     let firstName = "Manuel";
     console.log("Hallo, " + firstName + "!");
-
 }
 
 //console.log/firstName); // Fehler : scope!
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamen2("Markus"); // Argument --> Daten für Parameter
+ausgabeNamen2("Manuel");
+
+function ausgabeNamen2(firstName) { // Parameter
+    console.log("Hallo, " + firstName + "!");
+}

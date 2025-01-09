018461d6414304c4106c0bd7f2d9aea64bcaf4a7
PiSera1 <kevinwellens24@gmail.com>
2025-01-09 11:05:43 +0000

    09 / calculator / switch()

--- src/09_funktionen_2.js
+++ src/09_funktionen_2.js
@@ -4,21 +4,55 @@
 
 1. Dateneingabe + -überprüfung :  
 2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+3. Fkt. Grundrechenarten : check !
 4. Ausgabe in Konsole :  check!
 */
 
-const ERROR_STR_DIV = "Can't Divide by 0 ";
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+const ERROR_STR_CAL = "Can't divide by 0";
+const ERROR_STR_DIV = "Can't Divide by 0!";
 
-output(add(3,2));
-output(sub(10,7));
-output(mult(4,6));
-output(div(4,2));
-output(div(3,2));
-output(div(3,-2));
-output(div(0,2));
-output(div(3,0));
-output(div(0,0));
+
+
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+
+function calculator(a,b,op) {
+    switch (op) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return sub(a,b);
+        case "*":
+            return mult(a,b);
+        case "/":
+        case ":":
+            return div(a,b);
+        default:
+            return ERROR_STR_CAL;
+    }
+}
+
+
+
+
+
+// output(add(3,2));
+// output(sub(10,7));
+// output(mult(4,6));
+// output(div(4,2));
+// output(div(3,2));
+// output(div(3,-2));
+// output(div(0,2));
+// output(div(3,0));
+// output(div(0,0));
+
 
 
 // module: addition a + b | test:
@@ -42,22 +76,18 @@ function mult(a, b) {
 // module: division a / b | test:
 function div(a,b) {
 
-//     if (b == 0) {
-//         return ERROR_STR_DIV;
-//     }
-//     return a /b ;
-// }
-
-    if (b != 0) {
-       return a / b
-     } else {
+    if (b == 0) {
         return ERROR_STR_DIV;
-     }
- }
+    }
+    return a / b ;
+}
 
-
-
-
+//     if (b != 0) {
+//        return a / b
+//      } else {
+//         return ERROR_STR_DIV;
+//      }
+//  }
 
 // module: output | test:
 // output("hi");

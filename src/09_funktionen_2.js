5fea5dedf9a29d07a02d7f3befb83eccf960b43b
PiSera1 <kevinwellens24@gmail.com>
2025-01-09 09:18:31 +0000

    09 / division 1. Iteration

--- src/09_funktionen_2.js
+++ src/09_funktionen_2.js
@@ -9,25 +9,36 @@
 */
 
 
+output(add(3,2));
+output(sub(10,7));
+output(mult(4,6));
+output(div(4,2));
+output(div(3,2));
+output(div(3,-2));
+output(div(0,2));
+output(div(3,0));
+output(div(0,0));
+
 
 // module: addition a + b | test:
-output(add(3,2));
 
 function add(a, b) {
     return a + b;
 }
 
-//module: subtraction a - b | test:
+// module: subtraction a - b | test:
 
 function sub(a, b) {
     return a - b;
 }
 
-//module: multiplication a * b | test:
+// module: multiplication a * b | test:
 
 function mult(a, b) {
     return a * b;
 }
+
+// module: division a / b | test:
 
 function div(a, b) {
     return a / b;

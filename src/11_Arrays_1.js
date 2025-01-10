8e406af57350d0eab85ce1c1d159438d57d93ec7
PiSera1 <kevinwellens24@gmail.com>
2025-01-10 10:47:59 +0000

    01a. Funktionalität mit Array 1

--- src/11_Arrays_1.js
+++ src/11_Arrays_1.js
@@ -7,16 +7,16 @@
 
 /*** 02. Theorie: Array */
 
-let arr;
+// let arr;
 // arr = new Array(); // Konstruktor
-arr = []; // Literal
-arr = [2,11,7,8,10];
+// arr = []; // Literal
+// arr = [2,11,7,8,10];
 
 
-output(arr);
-output(arr.length); // Anzahl der Elemente
-output(arr[0]); // Index 0 (1. Post. im Array)
-output(arr[arr.length-1]); // letzte Pos. / allgemein
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Post. im Array)
+// output(arr[arr.length-1]); // letzte Pos. / allgemein
 
 
 /**
@@ -45,7 +45,21 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 }
 
 
+/*** 01a. Funktionalität mit Array 1 */
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
 
+    return str;
+}
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 

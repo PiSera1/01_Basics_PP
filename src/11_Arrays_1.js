eb1d61513687def3bad3b04d4dd05f1f37b23429
PiSera1 <kevinwellens24@gmail.com>
2025-01-10 11:44:11 +0000

    11 / 1. Variante: Struktur GENAU erzeugen ...

--- src/11_Arrays_1.js
+++ src/11_Arrays_1.js
@@ -20,31 +20,65 @@
 
 /* 02a. Theorie: Schleifen (for-schleife) */
 /* For -Schleifen als allg. Wiederholungs-Struktur */
+
 // Inkrement (untere Grenze --> obere Grenze)
 // for (let i = 0; i < 10; i++) 
 // {
 //     output("index: " + i);
 // }
+
 // Dekrement (obere Grenze --> untere Grenze)
 // for (let i = 10; i > 0; i--) 
 // {
 //     output("index: " + i);
 // }
-    
+
 // Inkrement (var. Schrittweite)
 // for (let i = 10; i <= 100; i += 5) 
 // {
 //     output("index: " + i);   
 // }
 
-
 /* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-for (let i = 0; i < arr.length; i++) 
-{
-   output(i); 
-   output(arr[i]);
-}
+
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) 
+// {
+//    output(i); 
+//    output(arr[i]);
+// }
+/********* Überlegungen - Transponierung **********/
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+/* Besser: mit FOR-Schleife */
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    a += 1; // a = a + 1
+//    output("inhalt von a: " + a);
+// }
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+// let str = ""; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//    str += "Test" + " "; // a = a + 1
+//    output(str);
+// }
+
 
 
 /**
@@ -57,19 +91,19 @@ for (let i = 0; i < arr.length; i++)
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4,word5,word6) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   word1 + GAP +
-                word2 + GAP +
-                word3 + GAP +
-                word4 + GAP +
-                word5 + GAP + 
-                word6 +
-                PUNCT;
+// // function getSentence(word1,word2,word3,word4,word5,word6) {
+// //     const GAP = " ";
+// //     const PUNCT = ".";
+//     // let str =   word1 + GAP +
+//     //             word2 + GAP +
+//     //             word3 + GAP +
+//     //             word4 + GAP +
+//     //             word5 + GAP + 
+//     //             word6 +
+//     //             PUNCT;
 
-    return str;
-}
+//     // return str;
+// }
 
 /*** 01a. Funktionalität mit Array 1 */
 // Kritik | Überlegung
@@ -91,11 +125,27 @@ function getSentenceArr(arr) {
     return str;
 }
 
-function getSentenceArr(arr) {
-    return arr.join(" ") + ".";
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =  "";
+
+     // 1. Variante: Struktur GENAU erzeugen ...
+     for (let i = 0; i < arr.length; i++) {
+        if (i != arr.length - 1) { // wenn du noch nicht am Ende des Arrays bist ...
+            str += arr[i] + GAP; // entweder ..
+        } else {
+            str += arr[i] + PUNCT;  // oder ...
+        }
+    }
+    return str;
 }
 
-output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+
 
 
 // Modul: Ausgabe in Konsole : Test

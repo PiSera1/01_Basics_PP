8e2ad4f9e05d90d9d50a022a66a2b6e1e185126e
PiSera1 <kevinwellens24@gmail.com>
2025-01-10 11:06:08 +0000

    11 / 02a. Theorie: Schleifen

--- src/11_Arrays_1.js
+++ src/11_Arrays_1.js
@@ -8,15 +8,37 @@
 /*** 02. Theorie: Array */
 
 // let arr;
-// arr = new Array(); // Konstruktor
+// // arr = new Array(); // Konstruktor
 // arr = []; // Literal
 // arr = [2,11,7,8,10];
 
-
 // output(arr);
 // output(arr.length); // Anzahl der Elemente
-// output(arr[0]); // Index 0 (1. Post. im Array)
-// output(arr[arr.length-1]); // letzte Pos. / allgemein
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]); // letze Pos. / allgemein
+
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 10; i++) 
+// {
+//     output("index: " + i);
+// }
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--) 
+// {
+//     output("index: " + i);
+// }
+    
+// Inkrement (var. Schrittweite)
+// for (let i = 10; i <= 100; i += 5) 
+// {
+//     output("index: " + i);   
+// }
+
+
+
 
 
 /**
@@ -29,7 +51,6 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-
 function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
@@ -37,16 +58,19 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
-                word5 + GAP +
+                word5 + GAP + 
                 word6 +
                 PUNCT;
 
     return str;
 }
 
-
 /*** 01a. Funktionalität mit Array 1 */
-output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
 function getSentenceArr(arr) {
     const GAP = " ";
     const PUNCT = ".";
@@ -54,12 +78,19 @@ function getSentenceArr(arr) {
                 arr[1] + GAP +
                 arr[2] + GAP +
                 arr[3] + GAP +
-                arr[4] + GAP +
+                arr[4] + GAP + 
                 arr[5] +
                 PUNCT;
 
     return str;
 }
+
+function getSentenceArr(arr) {
+    return arr.join(" ") + ".";
+}
+
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 

563f237217f8a2bd3479e34bdc3f536e48cc2362
PiSera1 <kevinwellens24@gmail.com>
2025-01-13 11:48:21 +0000

    14 Objekte 1 Daten/Funktionen

--- src/14_objects.js
+++ src/14_objects.js
@@ -8,29 +8,65 @@ function output(outputData) {
 /***** Erinnerung: Arrays *******/
 
 let arr;
-arr = ["Ich","bin","Max"];
-output(arr);
-output(arr[0]);
-output("------------");
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
 
 // nested arrays - Prinzip: [[[]]]
 // nested arrays  - hier: [[],[]]
-arr =   [
-            ["Ich","bin","Max"],
-            ["Ich","bin","Moritz"]
-        ];
+// arr =   [
+//             ["Ich","bin","Max"],
+//             ["Ich","bin","Moritz"]
+//         ];
       
-output(arr);
-output(arr[0]);
-output(arr[0][0]+ " " + arr[1][2]);
-output(arr[1][0]);
-output("------------");
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
+
+
+
+
+/***** Objekte 1 Daten/Funktionen *******/
+ 
+let person = {
+                firstName: "Kevin",
+                familyName: "Wellens",
+                salary: [120000, 160000,],
+                sayHello: function() {
+                    return "Hallo";
+                },
+                sayHello2: function() {
+                    return "Hallo, ich bin " + this.firstName;
+                }
+
+
+
+
+};
+
+
+output(person);
+output(person.firstName);
+output(person.familyName);
+output(person.salary[1]);
+output(person.sayHello());
+output(person.sayHello2());
+
+
+const txt = person.sayHello() + ", ich bin " + person.firstName +
+            " und verdiene " + person.salary[1] + " p.a."
+output(txt);
+
+
 
 // nested loop
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-        output("Index i: " + i + " Index j: " + j);
-        output(arr[i][j]);       
-    }
-}
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         output("Index i: " + i + " Index j: " + j);
+//         output(arr[i][j]);       
+//     }
+// }
 

34af255bbf4a8f4355d5442602ba32bf820dcebc
PiSera1 <kevinwellens24@gmail.com>
2025-01-13 08:34:23 +0000

    13 / Theorie push() / pop()

--- src/12_codechallenge_3.js
+++ src/12_codechallenge_3.js
@@ -16,27 +16,65 @@ output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
-function output(text) {
-    console.log(text);
-}
+// function output(text) {
+//     console.log(text);
+// }
 
-function getSentence(words, punctuationType) {
-    let sentence = words.join(' ');
-    let punctuation;
+// function getSentence(words, punctuationType) {
+//     let sentence = words.join(' ');
+//     let punctuation;
 
-    switch (punctuationType) {
-        case 'S':
-            punctuation = '.';
-            break;
-        case 'Q':
-            punctuation = '?';
-            break;
-        case 'E':
-            punctuation = '!';
-            break;
-        default:
-            punctuation = '.';
+//     switch (punctuationType) {
+//         case 'S':
+//             punctuation = '.';
+//             break;
+//         case 'Q':
+//             punctuation = '?';
+//             break;
+//         case 'E':
+//             punctuation = '!';
+//             break;
+//         default:
+//             punctuation = '.';
+//     }
+
+//     return sentence + punctuation;
+// }
+
+
+function getSentence(arr,op) {
+    const GAP = " ";
+    const PUNCT = getPunct(op);  // . ? !
+    let str = ""; 
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i != arr.length-1) {
+            str += arr[i] + GAP;
+        } else {
+            str += arr[i] + PUNCT; 
+        }
     }
-
-    return sentence + punctuation;
+     return str;
 }
+
+
+function getPunct(op) {
+    switch (op) {
+        case "S":
+           return "."; 
+        case "Q":
+            return "?";
+        case "E":
+            return "!";
+        default:
+            return "#";
+    }
+}
+
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(outputData) {
+    console.log(outputData);
+}
+
